9c6582cea1f006e48f61018be8cf21c8b380048b
xx-owen-xx <fisselowen@gmail.com>
2025-06-24 12:30:25 -0700

    Update temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -4,7 +4,14 @@
 # task: prompt user to input a temperature in celcius and then convert that temperature to fahrenheit
  
 
-C = float(input('Please enter a Celsius temperature.\n'))
-F = (9 / 5) * C + 32
+#prompts user to input 5 numbers
+num_1 = float(input('input one number: '))
+num_2 = float(input('input one number: '))
+num_3 = float(input('input one number: '))
+num_4 = float(input('input one number: '))
+num_5 = float(input('input one number: '))
 
-print('The equivalent Fahrenheit temperature is:\n' + str(F))
+#divides the inputs by 5 (the number of inputs) giving us the mean or average
+average =((num_1 + num_2 + num_3 + num_4 + num_5) / 5)
+#prints the result (average)
+print('The average of those numbers:\n',(average))
