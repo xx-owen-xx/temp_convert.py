c6e0a3b1a090c367d13d13e2bd296260b774199d
xx-owen-xx <fisselowen@gmail.com>
2025-06-24 11:39:19 -0700

    Update temp_convert.py

--- temp_convert.py
+++ temp_convert.py
@@ -4,7 +4,7 @@
 # task: prompt user to input a temperature in celcius and then convert that temperature to fahrenheit
  
 
-C = float(input('Please enter a Celsius temperature.'))
+C = float(input('Please enter a Celsius temperature.\n'))
 F = (9 / 5) * C + 32
 
-print('The equivalent Fahrenheit temperature is:' , F)
+print('The equivalent Fahrenheit temperature is:\n' , F)
