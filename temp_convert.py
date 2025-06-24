317526ef28f7a9d80a1ea5a03bafe516ce516c03
xx-owen-xx <fisselowen@gmail.com>
2025-06-24 11:32:33 -0700

    Add files via upload

---
+++ temp_convert.py
@@ -0,0 +1,10 @@
+# Author: Owen Fissel
+# username: xx-owen-xx
+# date: 06/23/25
+# task: prompt user to input a temperature in celcius and then convert that temperature to fahrenheit
+ 
+
+C = float(input('Please enter a Celsius temperature.'))
+F = (9 / 5) * C + 32
+
+print('The equivalent Fahrenheit temperature is:' , F)
